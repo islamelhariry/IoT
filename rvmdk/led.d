3a7b4b7a88bc309fcec09b8c490c1959b4f94608
islamelhariry <islam.elhariry@yahoo>
2024-01-23 21:19:27 +0000

    first commit

---
+++ rvmdk/led.d
@@ -0,0 +1,3 @@
+.\rvmdk\led.o: LED.c
+.\rvmdk\led.o: E:\Campus\Courses\Embedded\TM4C123GXL\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\rvmdk\led.o: ..\inc/tm4c123gh6pm.h
---
+++ rvmdk/ustdlib.d
@@ -0,0 +1,6 @@
+.\rvmdk\ustdlib.o: ..\utils\ustdlib.c
+.\rvmdk\ustdlib.o: E:\Campus\Courses\Embedded\TM4C123GXL\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\rvmdk\ustdlib.o: ..\driverlib/debug.h
+.\rvmdk\ustdlib.o: ..\utils/ustdlib.h
+.\rvmdk\ustdlib.o: E:\Campus\Courses\Embedded\TM4C123GXL\Keil\ARM\ARMCC\bin\..\include\stdarg.h
+.\rvmdk\ustdlib.o: E:\Campus\Courses\Embedded\TM4C123GXL\Keil\ARM\ARMCC\bin\..\include\time.h
